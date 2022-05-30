09b9cae1bb7ac3f0a73f619d49010da7785e59be
unknown <zaeemaftabsiddiqui@gmail.com>
2022-05-30 20:38:28 +0500

    comment script , delete tamplate and add nav bar

--- pages/index.js
+++ pages/index.js
@@ -12,42 +12,34 @@ export default function Home() {
         <meta name="Keywords" content="nextjs, personal blog, blog " />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <script src="/sc.js" strategy="lazyOnload"></script>
+      {/* <script src="/sc.js" strategy="lazyOnload"></script> */}
+      <nav className={styles.mainnav}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
       <main className={styles.main}>
         <h1 className={styles.title}>Welcome to personal Blog</h1>
 
         <p className={styles.description}>
           A blog for students by Best teacher
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="blogs">
+          <div className="BlogItem">
+            <h1>How to learn javascript in 2022?</h1>
+            <p>Javascript is the language</p>
+          </div>
+          <div className="BlogItem">
+            <h1>How to learn javascript in 2022?</h1>
+            <p>Javascript is the language</p>
+          </div>
+          <div className="BlogItem">
+            <h1>How to learn javascript in 2022?</h1>
+            <p>Javascript is the language</p>
+          </div>
         </div>
       </main>
 
