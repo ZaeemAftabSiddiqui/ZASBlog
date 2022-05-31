eb5f8ad81631e3ac277b6788263560568ff4c1a5
unknown <zaeemaftabsiddiqui@gmail.com>
2022-05-31 20:51:12 +0500

    add image and build Navbar component

---
+++ Components/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <nav className={styles.mainnav}>
+      <ul>
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/About">
+          <a>
+            <li>About</li>
+          </a>
+        </Link>
+        <Link href="/Blog">
+          <a>
+            <li>Blog</li>
+          </a>
+        </Link>
+        <Link href="/Contact">
+          <a>
+            <li>Contact</li>
+          </a>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
