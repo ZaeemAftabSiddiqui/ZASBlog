80e21a6e030a1aa1ca115a4a63da40f043443e30
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-08-23 16:08:23 +0500

    Using the API Endpoints

--- pages/Blog.js
+++ pages/Blog.js
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Blog.module.css";
 import Link from "next/link";
-
 //Step 1 : Collect all the files from blogdata directory
 //Step 2: Iterate through them and display them
 
 const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    console.log("use effect is running");
+    fetch("http://localhost:3000/api/Blogs")
+      .then((a) => {
+        return a.json();
+      })
+      .then((parsed) => {
+        console.log(parsed);
+        setBlogs(parsed);
+      });
+  }, []);
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div>
-          <Link href={"/BlogPost/learn-javascript"}>
-            <h3 className={styles.BlogItemh3}>
-              How to learn javascript in 2022?
-            </h3>
-          </Link>
-          <p>Javascript is the language</p>
-        </div>
-        <div className="BlogItem">
-          <h3>How to learn javascript in 2022?</h3>
-          <p>Javascript is the language</p>
-        </div>
-        <div className="BlogItem">
-          <h3>How to learn javascript in 2022?</h3>
-          <p>Javascript is the language</p>
-        </div>
+        {blogs.map((blogitem) => {
+          return (
+            <div key={blogitem.slug}>
+              <Link href={`/BlogPost/${blogitem.slug}`}>
+                <h3 className={styles.BlogItemh3}>{blogitem.title}</h3>
+              </Link>
+              <p className={styles.BlogItemp}>
+                {blogitem.content.substr(0, 140)}...
+              </p>
+            </div>
+          );
+        })}
       </main>
     </div>
   );
