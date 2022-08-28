920a4154c4008378415e1f7592a1229b0812e238
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-08-28 16:03:41 +0500

    Sample Designing the About Page

--- pages/About.js
+++ pages/About.js
@@ -1,7 +1,46 @@
 import React from "react";
+import styles from "../styles/About.module.css";
 
 const About = () => {
-  return <div>This is my about component </div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.center}>About ZAS-blog </h1>
+      <h2>Introduction</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed
+        dignissimos quam illo tempora nam eius repudiandae nostrum, culpa dolore
+        quae repellat. Magnam hic porro, eos omnis voluptates sit perspiciatis
+        exercitationem.
+      </p>
+      <h2>Services offered</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet odio
+        aliquid aperiam. Deserunt perspiciatis facere molestias exercitationem,
+        quos labore, fugiat autem quae impedit modi, suscipit tempore incidunt
+        ullam qui quis laudantium odit culpa reprehenderit.
+      </p>
+      <p>We offer following services</p>
+      <ul>Services 1</ul>
+      <ul>Services 1</ul>
+      <ul>Services 1</ul>
+      <ul>Services 1</ul>
+      <ul>Services 1</ul>
+      <ul>Services 1</ul>
+      <ul>Services 1</ul>
+      <ul>Services 1</ul>
+
+      <h2>Contact us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+        exercitationem esse tenetur nesciunt eveniet, ipsum doloribus fuga
+        quibusdam maxime! Suscipit corporis molestias, eum quae, dolorem
+        consequuntur voluptate, modi voluptatibus rerum expedita odio eligendi
+        officiis exercitationem. Quos aliquam, ea totam ad nemo, fuga ex iste ab
+        doloribus quas natus nisi sed labore dolores debitis quis nesciunt
+        consectetur.
+      </p>
+    </div>
+  );
 };
 
 export default About;
